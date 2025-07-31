bffdaf59d5d200a44264b2988d2e1aae8965ecb9
sturgeonitllc-cell <sturgeonit.llc@gmail.com>
2025-07-31 11:22:49 -0400

    Update airtable.js

--- netlify/functions/airtable.js
+++ netlify/functions/airtable.js
@@ -1,1 +1,24 @@
+const axios = require('axios');
 
+exports.handler = async function(event, context) {
+    try {
+        const { data } = await axios.get(
+            `https://api.airtable.com/v0/${process.env.ATRTABLE_BASE_ID}/${process.env.ATRTABLE_TABLE_NAME}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${process.env.ATRTABLE_API_KEY}`
+                }
+            }
+        );
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.records)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to fetch data' })
+        };
+    }
+};
